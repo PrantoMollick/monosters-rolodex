f165d4f28d3699b264f4b510b0527115beb7f3ae
PrantoMollick <prantocse01@gmail.com>
2022-03-05 14:13:40 +0600

    Updates

---
+++ precache-manifest.78ee4d4a0ac9e3a7fefa9d90526c3131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d4451d10c81693401f",
+    "url": "/monosters-rolodex/static/css/main.057a1abc.chunk.css"
+  },
+  {
+    "revision": "98d4451d10c81693401f",
+    "url": "/monosters-rolodex/static/js/main.5f19ea82.chunk.js"
+  },
+  {
+    "revision": "4e971c2e73f9e49b836a",
+    "url": "/monosters-rolodex/static/js/runtime~main.f3430c4f.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/monosters-rolodex/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/monosters-rolodex/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "4ebb58cf72d526436041b107c08c289f",
+    "url": "/monosters-rolodex/index.html"
+  }
+];
